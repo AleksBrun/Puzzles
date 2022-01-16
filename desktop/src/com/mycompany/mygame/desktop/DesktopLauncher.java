e0aa5caef0a38c4324d3a2871c2ac0aa8748ba03
Aleks Brun <bas1978@bk.ru>
2022-01-16 09:36:03 +0400

    New Game Puzzles

---
+++ desktop/src/com/mycompany/mygame/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.mycompany.mygame.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.mycompany.mygame.MyGdxGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new MyGdxGame(), config);
+	}
+}
